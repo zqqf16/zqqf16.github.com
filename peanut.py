d01943b647c4996346a232fcaa8612b5f2eaac06
zorro <zqqf16@gmail.com>
2013-08-19 16:42:48 +0800

    complete

--- peanut.py
+++ peanut.py
@@ -7,217 +7,27 @@ from mako.lookup import TemplateLookup
 from datetime import datetime
 from markdown2 import markdown
 
-config = {
-    'title': u'穷折腾',
-    'path': {
-        'draft': 'drafts/',
-        'post': 'posts/',
-        'page': 'pages/',
-        'tag': 'tags/',
-        'rss': 'rss.xml',
-        'sitemap': 'sitemap.xml',
-    },
+CONFIG = {
     'domain': 'zqqf16.info',
+    'title': u'穷折腾',
+    'description': u'des',
 }
 
-_DRAFT_PATH = config['path']['draft']
+PATH = {
+    'draft':    'drafts',
+    'post':     'posts',
+    'page':     'pages',
+    'tag':      'tags',
+}
 
-templates = TemplateLookup(
-    directories=['templates'], 
-    input_encoding='utf-8',
-    output_encoding='utf-8', 
-    encoding_errors='replace'
-)
-
-class Tag():
-    template = templates.get_template('tag.html')
-
-    def __init__(self, name):
-        self.name = name
-        self.posts = []
-        self.path = config['path']['tag'] + self.name
-
-    def generate(self, info):
-        ''' Generate html file '''
-
-        if not os.path.exists(self.path):
-            os.mkdir(self.path)
-
-        filepath = os.path.join(self.path, 'index.html')
-        with open(filepath, 'w') as f: 
-            self.posts.sort(lambda x, y: cmp(x.date, y.date), reverse=True)
-            html = self.template.render(info=info, tag=self, posts=self.posts)
-            f.write(html)
-
-class Blog():
-    template = templates.get_template('post.html')
-
-    def __init__(self):
-        self.__slug = ''
-        self.tags = []
-        self.title = ''
-        self.date = ''
-        self.category = None
-        self.type = 'post'
-
-    def __get_meta_handler(self, meta):
-        handlers = {
-            'tags':     lambda x: [Tag(t.strip(' \n')) for t in x.split(',')],
-            'title':    lambda x: x,
-            'date':     lambda x: datetime.strptime(x, '%Y-%m-%d'),
-            'category': lambda x: x,
-            'type':     lambda x: x,
-        }
-        try:
-            return handlers[meta]
-        except:
-            return lambda x: x
-
-    def __parse_metadata(self):
-        self.tags = []
-        self.title = ''
-        self.date = datetime.now()
-        self.category = None
-        self.type = 'post'
-
-        for key, value in self.__meta.items():
-            self.__dict__[key] = self.__get_meta_handler(key)(value)
-
-        path = config['path']['post'] if self.type=='post' else config['path']['page']
-        self.path = path + self.__slug + '.html'
-
-    def read(self, filepath):
-        ''' Read blog content and metadata from file'''
-
-        file_name_re = re.compile(r'([^/]+)\.(md|markdown)')
-        m = file_name_re.search(filepath)
-        if not m: 
-            return
-
-        self.__slug = m.group(1)
-
-        with open(filepath, 'r') as f:
-            content = f.read().decode('utf-8')
-            self.content = markdown(content.strip(' \n'), 
-                                 extras=["fenced-code-blocks", "metadata"])
-            self.__meta = self.content.metadata
-            self.__parse_metadata()
-
-    def generate(self, info):
-        ''' Generate html file '''
-
-        with open(self.path, 'w') as f: 
-            html = self.template.render(info=info, post=self)
-            f.write(html)
-
-class Peanut():
-    def __init__(self, path):
-        self.path = path
-        self.info = {
-            'posts': [],
-            'pages': [],
-            'tags': [],
-            'domain': config['domain'],
-            'title': config['title'],
-        }
-
-    def load(self, draft_path=_DRAFT_PATH):
-        '''Load blogs from files'''
-
-        posts = []
-        pages = []
-        tags = {}
-
-        path = self.path+'/'+draft_path
-        for f in os.listdir(path):
-            blog = Blog()
-            blog.read(path+'/'+f)
-
-            if blog.type == 'post':
-                posts.append(blog)
-            elif blog.type == 'page':
-                pages.append(blog)
-                continue
-
-            try:
-                for tag in blog.tags:
-                    name = tag.name
-                    if tags.get(name):
-                        tags[name].posts.append(blog)
-                    else:
-                        t = Tag(name)
-                        t.posts.append(blog)
-                        tags[name] = t
-            except:
-                pass
-
-        posts.sort(lambda x, y: cmp(x.date, y.date), reverse=True)
-        self.info['posts'] = posts
-        self.info['pages'] = pages
-        self.info['tags'] = tags
-
-    def gen_html_posts(self):
-        for p in self.info['posts']:
-            p.generate(self.info)
-
-    def gen_html_pages(self):
-        for p in self.info['pages']:
-            p.generate(self.info)
-
-    def gen_html_tags(self):
-        for n, t in self.info['tags'].items():
-            t.generate(self.info)
-
-    def gen_html_index(self):
-        template = templates.get_template('index.html')
-        with open('index.html', 'w') as f: 
-            html = template.render(info=self.info, posts=self.info['posts'])
-            f.write(html)
-
-    def gen_xml_sitemap(self):
-        template = templates.get_template('sitemap.xml')
-        with open('sitemap.xml', 'w') as f:
-            xml = template.render(info=self.info, pages=self.info['pages'], posts=self.info['posts'])
-            f.write(xml)
-
-    def gen_xml_rss(self):
-        template = templates.get_template('rss.xml')
-        with open('rss.xml', 'w') as f:
-            xml = template.render(info=self.info, posts=self.info['posts'][:5])
-            f.write(xml)
-
-    def gen_all(self):
-        self.gen_html_posts()
-        self.gen_html_pages()
-        self.gen_html_tags()
-        self.gen_html_index()
-        self.gen_xml_sitemap()
-        self.gen_xml_rss()
-
-#MVC start
-
-'''
-    M -- post/tag/page
-    V -- template
-    C -- peanut
-
-    Usage:
-    posts = Post.find_all()
-    tags = Tag.find_all()
-    pages = Page.find_all()
-
-    for tag in post.tags:
-        pass
-    for post in tag.posts:
-        pass
-'''
-_DRAFT_DIR = 'draft'
+def get_url(type, name):
+    return PATH[type]+'/'+name+'.html'
 
 class Tag(object):
     def __init__(self, name):
         self.name = name
         self.posts = []
-        self.url = 'tag_url'
+        self.url = get_url('tag', self.name)
 
 class Post(object):
     def __init__(self, title, content, slug, date=None, tags=[]):
@@ -234,6 +44,8 @@ class Post(object):
             tag.posts.append(self)
             self.tags.append(tag)
 
+        self.url = get_url('post', self.slug)
+
 class Page(object):
     def __init__(self, title, content, slug, date=None):
         self.title = title
@@ -244,17 +56,28 @@ class Page(object):
         else:
             self.date = datetime.now()
 
-class Database(object):
-    #TODO must singleton
+        self.url = get_url('page', self.slug)
+
+class Draft(object):
     file_name_re = re.compile(r'([^/]+)\.(md|markdown)')
+    _instance = None
 
-    def __init__(self, directory = _DRAFT_DIR):
+    def __new__(cls, *args, **kwargs):
+        #singleton
+        if not isinstance(cls._instance, Draft):
+            cls._instance = super(Draft, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
+        
+    def __init__(self, directory = PATH['draft']):
         self.directory = directory
-        self.posts = []
-        self.pages = []
-        self.tags = {}
+        self._data = {
+            'posts': [],
+            'pages': [],
+            'tags':  {},
+        }
 
-    _META_HANDLER = {
+    # metadata handlers
+    _META_HANDLERS = {
         'tags':     lambda x: [t.strip(' \n') for t in x.split(',')] if x else [],
         'title':    lambda x: x if x else '',
         'date':     lambda x: datetime.strptime(x, '%Y-%m-%d') if x else datetime.now(),
@@ -264,24 +87,31 @@ class Database(object):
 
     def __parse_metadata(self, meta):
         res = {}
-        for name, handler in self._META_HANDLER.items():
+        for name, handler in self._META_HANDLERS.items():
             value = handler(meta.get(name))
             res[name] = value
 
         return res
 
-    def get_tags(self, name_list):
+    def __get_tags(self, name_list):
+        ''' If tag name exists in self._data['tags'], return it.
+            Else create a new tag and return it.
+        '''
         tags = []
         for name in name_list:
-            if self.tags.get(name):
-                tags.append(self.tags[name])
+            tag = self._data['tags'].get(name)
+            if tag:
+                tags.append(tag)
             else:
                 tag = Tag(name)
-                self.tags[name] = tag
+                self._data['tags'][name] = tag
                 tags.append(tag)
         return tags
 
     def load(self):
+        ''' Parse draft files to generate posts, pages and tags.
+            Draft file should be named as 'xxx.md' or 'xxx.markdown'.
+        '''
         for f in os.listdir(self.directory):
             m = self.file_name_re.search(f)
             if not m: 
@@ -297,20 +127,78 @@ class Database(object):
                     #No need to convert this draft
                     continue
                 meta = self.__parse_metadata(html.metadata)
-                tags = self.get_tags(meta['tags'])
+                tags = self.__get_tags(meta['tags'])
 
                 if meta['type'] == 'post':
                     post = Post(meta['title'], html, slug,
                                 meta['date'], tags)
-                    self.posts.append(post)
+                    self._data['posts'].append(post)
                 elif meta['type'] == 'page':
                     page = Page(meta['title'], html, slug,
                                 meta['date'])
-                    self.pages.append(page)
-        
-        self.posts.sort(lambda x, y: cmp(x.date, y.date), reverse=True)
+                    self._data['pages'].append(page)
+
+        self._sort_posts_by_date()
+    
+    def _sort_posts_by_date(self):
+        self._data['posts'].sort(lambda x, y: cmp(x.date, y.date), reverse=True)
+
+    @property
+    def posts(self):
+        return self._data['posts']
+
+    @property
+    def pages(self):
+        return self._data['pages']
+
+    @property
+    def tags(self):
+        return [value for name,value in self._data['tags'].items()]
+
+_TEMPLATES = TemplateLookup(
+    directories=['templates'], 
+    input_encoding='utf-8',
+    output_encoding='utf-8', 
+    encoding_errors='replace'
+)
+
+def generate_file(template, path, **namespace):
+    with open(path, 'w') as f: 
+        html = template.render(**namespace)
+        f.write(html)
+
+def peanut():
+    drafts = Draft()
+    drafts.load()
+    tags = drafts.tags
+    posts = drafts.posts
+    pages = drafts.pages
+
+    post_temp = _TEMPLATES.get_template('post.html')
+    page_temp = _TEMPLATES.get_template('page.html')
+    tag_temp =  _TEMPLATES.get_template('tag.html')
+    index_temp = _TEMPLATES.get_template('index.html')
+    rss_temp = _TEMPLATES.get_template('rss.xml')
+    sitemap_temp = _TEMPLATES.get_template('sitemap.xml')
+    
+    namespace = {
+        'tags': tags,
+        'posts': posts,
+        'pages': pages,
+    }
+
+    namespace.update(CONFIG)
+
+    for tag in tags:
+        generate_file(tag_temp, tag.url, tag=tag, **namespace)
+    for post in posts:
+        generate_file(post_temp, post.url, post=post, **namespace)
+    for page in pages:
+        generate_file(page_temp, page.url, page=page, **namespace)
+
+    generate_file(index_temp, 'index.html', **namespace)
+    generate_file(rss_temp, 'rss.xml', **namespace)
+    generate_file(sitemap_temp, 'sitemap.xml', **namespace)
 
 if __name__ == '__main__':
-    p = Peanut('.')
-    p.load()
-    p.gen_all()
+    peanut()
